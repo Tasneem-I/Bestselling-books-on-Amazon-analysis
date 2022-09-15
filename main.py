9867b295770a552fd499635285c0d4ca8da2fc6c
Tasneem <107906533+Tasneem-I@users.noreply.github.com>
2022-09-15 11:36:36 +0000

    Made another scatter plot

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
-azn_books= pd.read_csv("dataset.csv")
-print(azn_books.head()) 
+Amazon_books= pd.read_csv("dataset1.csv")
+print(Amazon_books.head()) 
+#graph 1
+
 
 #comparing the amount of non fiction and fiction books that have appeared as bestsellers 
-genre_type = azn_books["Genre"].unique()
-nonfic_count = azn_books["Genre"].value_counts()['Non Fiction']
-fic_count = azn_books["Genre"].value_counts()['Fiction']
+genre_type = Amazon_books["Genre"].unique()
+nonfic_count = Amazon_books["Genre"].value_counts()['Non Fiction']
+fic_count = Amazon_books["Genre"].value_counts()['Fiction']
 count_comparison= pd.DataFrame({'count': [nonfic_count, fic_count], "book genre": genre_type})
 
 # bar graph for comparing the no of fiction and non fiction books that made it to the bestsellers between the years 2009-2021
 count_comparison_graph = count_comparison.plot.bar(x="book genre", y= "count", color=['red', 'green']) 
+
+
+
+#graph 2
+
+plt.scatter(Amazon_books[" user_ratings"], Amazon_books["price_r"])
